0a3c4380afb0ccbcb3f68d4fb95252f7a3486b2d
git config --global LakshanEdirisinghe <Ayomall30@gmail.com>
2026-06-24 21:29:37 +0530

    Logo name was changed

--- src/assets/Components/navBar/NavBar.jsx
+++ src/assets/Components/navBar/NavBar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function NavBar() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <nav class="navbar navbar-expand-lg navColor">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
+          <a class="navbar-brand" href="#">OnaVedak.com</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
