f41a78f83ce26ceffb0d052119d4207ac4527faf
git config --global LakshanEdirisinghe <Ayomall30@gmail.com>
2026-07-29 11:14:11 +0530

    Enhance hero section styles and layout; refactor navbar styles for improved functionality (nav bar fixed-top removed)

--- components/navbar.js
+++ components/navbar.js
@@ -704,7 +704,7 @@ export function initializeMegaMenu() {
 export function navBarNormal() {
     return `
 
-    <nav class="navbar navbar-expand-lg fixed-top navBg-color" >
+    <nav class="navbar navbar-expand-lg  navBg-color" >
         <div class="container-fluid">
 
             <!-- Brand -->
